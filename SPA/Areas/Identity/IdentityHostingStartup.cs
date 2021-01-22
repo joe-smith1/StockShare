d75edc560540cd2fb38e5bb99c2ead3c772e4fba
Joe <smithjose4@myvuw.ac.nz>
2021-01-22 15:36:47 +1300

    Scafolding added for login, logout and register pages

---
+++ SPA/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SPA.Data;
+using SPA.Models.Entities;
+
+[assembly: HostingStartup(typeof(SPA.Areas.Identity.IdentityHostingStartup))]
+namespace SPA.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
